50a366d0b51b9a46780cf78caafee80ba844aaed
Esben-Andreas-Madsen <mainmaxen@gmail.com>
2026-01-08 14:08:37 +0100

    Initial commit

---
+++ spring-boot-library-backend/src/main/java/io/github/eama/spring_boot_library_backend/SpringBootLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.eama.spring_boot_library_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryBackendApplication.class, args);
+	}
+
+}
---
+++ spring-boot-library-backend/src/test/java/io/github/eama/spring_boot_library_backend/SpringBootLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eama.spring_boot_library_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
